f8a5dc5e3d1d35d65f1a4c5bea88ad9c0a55310d
Sambit Tarai <sambitarai17@gmail.com>
2020-12-02 16:07:03 +0530

    utils updated

--- util/utils.py
+++ util/utils.py
@@ -19,3 +19,25 @@ def xyz2irc(coord_xyz, origin_xyz, vxSize_xyz, direction_a):
 	cri_a = ((coord_a - origin_a) @ np.linalg.inv(direction_a)) / vxSize_a
 	cri_a = np.round(cri_a)
 	return IrcTuple(int(cri_a[2]), int(cri_a[1]), int(cri_a[0]))
+
+def importstr(module_str, from_=None):
+    """
+    >>> importstr('os')
+    <module 'os' from '.../os.pyc'>
+    >>> importstr('math', 'fabs')
+    <built-in function fabs>
+    """
+    if from_ is None and ':' in module_str:
+        module_str, from_ = module_str.rsplit(':')
+
+    module = __import__(module_str)
+    for sub_str in module_str.split('.')[1:]:
+        module = getattr(module, sub_str)
+
+    if from_:
+        try:
+            return getattr(module, from_)
+        except:
+            raise ImportError('{}.{}'.format(module_str, from_))
+    return module
+
