25fa02f125d1ddae1edf881333261b640b36d333
Sambit Tarai <44292855+sambittarai@users.noreply.github.com>
2020-12-02 20:19:20 +0530

    Update util.py

--- util/util.py
+++ util/util.py
@@ -41,3 +41,70 @@ def importstr(module_str, from_=None):
             raise ImportError('{}.{}'.format(module_str, from_))
     return module
 
+def prhist(ary, prefix_str=None, **kwargs):
+    if prefix_str is None:
+        prefix_str = ''
+    else:
+        prefix_str += ' '
+
+    count_ary, bins_ary = np.histogram(ary, **kwargs)
+    for i in range(count_ary.shape[0]):
+        print("{}{:-8.2f}".format(prefix_str, bins_ary[i]), "{:-10}".format(count_ary[i]))
+    print("{}{:-8.2f}".format(prefix_str, bins_ary[-1]))
+
+def enumerateWithEstimate(
+        iter,
+        desc_str,
+        start_ndx=0,
+        print_ndx=4,
+        backoff=None,
+        iter_len=None,
+):
+
+    if iter_len is None:
+        iter_len = len(iter)
+
+    if backoff is None:
+        backoff = 2
+        while backoff ** 7 < iter_len:
+            backoff *= 2
+
+    assert backoff >= 2
+    while print_ndx < start_ndx * backoff:
+        print_ndx *= backoff
+
+    log.warning("{} ----/{}, starting".format(
+        desc_str,
+        iter_len,
+    ))
+    start_ts = time.time()
+    for (current_ndx, item) in enumerate(iter):
+        yield (current_ndx, item)
+        if current_ndx == print_ndx:
+            # ... <1>
+            duration_sec = ((time.time() - start_ts)
+                            / (current_ndx - start_ndx + 1)
+                            * (iter_len-start_ndx)
+                            )
+
+            done_dt = datetime.datetime.fromtimestamp(start_ts + duration_sec)
+            done_td = datetime.timedelta(seconds=duration_sec)
+
+            log.info("{} {:-4}/{}, done at {}, {}".format(
+                desc_str,
+                current_ndx,
+                iter_len,
+                str(done_dt).rsplit('.', 1)[0],
+                str(done_td).rsplit('.', 1)[0],
+            ))
+
+            print_ndx *= backoff
+
+        if current_ndx + 1 == start_ndx:
+            start_ts = time.time()
+
+    log.warning("{} ----/{}, done at {}".format(
+        desc_str,
+        iter_len,
+        str(datetime.datetime.now()).rsplit('.', 1)[0],
+    ))
