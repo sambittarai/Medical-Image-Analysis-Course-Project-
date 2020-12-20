a62ed05da88bd5d926521f231f1fa916fd463e6c
Sambit Tarai <44292855+sambittarai@users.noreply.github.com>
2020-12-20 21:17:51 +0530

    Update disk.py

--- util/disk.py
+++ util/disk.py
@@ -1,7 +1,6 @@
 import gzip
 from diskcache import FanoutCache, Disk
 from diskcache.core import BytesType, MODE_BINARY, BytesIO
-
 from util.logconf import logging
 log = logging.getLogger(__name__)
 # log.setLevel(logging.WARN)
